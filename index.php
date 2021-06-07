eb15b7910b8e7cba05d99456fa99e799ec711ced
R3VANEK <janek2000-2000@wp.pl>
2021-06-07 17:24:37 +0200

    experimenting with ::before

--- index.php
+++ index.php
@@ -5,32 +5,84 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>treeFiles</title>
+
+
+    <style>
+
+        *{
+            margin:0;
+            padding:0;
+            position: relative;
+        }
+        a{
+            text-decoration:none;
+           
+        }
+
+        .test{
+            position: absolute;
+            width:2px;
+            height:18px;
+            background-color:black;
+            top:54px;
+            left:100px;
+        }
+
+        a::before{
+            content:'';
+            position: absolute;
+            background-color:black;
+            height:3px;
+            width:30px;
+            top:50%;
+            left:-5px;
+        }
+        p::before{
+            content:'';
+            position: absolute;
+            background-color:black;
+            height:3px;
+            width:30px;
+            top:50%;
+            left:-5px;
+        }
+
+    </style>
 </head>
 <body>
     
 
-    <?php
-    
-        function recursive_scan($given_dir, $margin_value){
+        <?php
 
-            $scanned_dir = scandir($given_dir);
 
-            for($i = 2; $i < count($scanned_dir); $i++){
+            // zapisywać parenta folderu albo pliku?
+                
+            function recursive_scan($given_dir, $margin_value){
 
-                if(substr($scanned_dir[$i], -4, -3) == '.'){
-                    $href = $given_dir.'/'.$scanned_dir[$i];
-                    echo "<a href='$href' style='margin-left: $margin_value%'>".$scanned_dir[$i]."</a>";
+                $scanned_dir = scandir($given_dir);
+
+                $help_margin = $margin_value.'px';
+
+                for($i = 2; $i < count($scanned_dir); $i++){
+
+                    if(substr($scanned_dir[$i], -4, -3) == '.'){
+                        $href = $given_dir.'/'.$scanned_dir[$i];
+                        echo "<a href='$href' style='margin-left: $help_margin'>".'-----'.$scanned_dir[$i]."</a><br/>";
+                    }
+                    else{
+                        echo "<p style='margin-left : $help_margin'>".'-----'.$scanned_dir[$i]."</p>";
+                        recursive_scan($given_dir.'/'.$scanned_dir[$i], $margin_value + 50);
+                    }
+
                 }
-                else{
-                    echo "<p style='margin-left : $margin_value%'>".$scanned_dir[$i]."</p>";
-                    recursive_scan($given_dir.'/'.$scanned_dir[$i], $margin_value + 5);
-                }
+            };
 
-            }
-        };
+            recursive_scan("./exampleRootFolder", 0);
 
-        recursive_scan("./exampleRootFolder", 0);
-    ?>
+
+        ?>
+
+        <div class="test"></div>
     
 </body>
 </html>
